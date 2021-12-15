d52d489a42f017582fb46da1a1b02d6efa47ee4f
ashwinivishwaspine <87471129+ashwinivishwaspine@users.noreply.github.com>
2021-12-15 13:59:38 +0530

    Initial Commit

---
+++ PluralPay/PluralPay.h
@@ -0,0 +1,18 @@
+//
+//  PluralPay.h
+//  PluralPay
+//
+//  Created by Ashwini Vishwas on 15/12/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for PluralPay.
+FOUNDATION_EXPORT double PluralPayVersionNumber;
+
+//! Project version string for PluralPay.
+FOUNDATION_EXPORT const unsigned char PluralPayVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PluralPay/PublicHeader.h>
+
+
